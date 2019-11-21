42850a8627ee37617839216ab852da46bf79fca4
Olanrewaju <lanrehnics@kobo360.com>
2019-11-21 14:43:37 +0100

    --

--- lib/mobile_map_core.dart
+++ lib/mobile_map_core.dart
@@ -4,7 +4,6 @@ import 'package:flutter/services.dart';
 
 class MobileMapCore {
   static const MethodChannel _channel = const MethodChannel('mobile_map_core');
-
   static Future<String> get prepareMap async {
     final String status = await _channel.invokeMethod('prepareMap');
     return status;
