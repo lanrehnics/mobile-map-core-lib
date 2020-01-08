d85be78c23bb4413d3a2b05ab25bcd09375119fb
Olanrewaju <lanrehnics@kobo360.com>
2020-01-08 10:59:31 +0100

    --

--- lib/mobile_map_core.dart
+++ lib/mobile_map_core.dart
@@ -3,8 +3,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class MobileMapCore {
-  static const MethodChannel _channel =
-      const MethodChannel('mobile_map_core');
+  static const MethodChannel _channel = const MethodChannel('mobile_map_core');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
