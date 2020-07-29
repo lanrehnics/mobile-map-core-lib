ba7fb3cbf59a039d514730f120024071081e9eb3
Olanrewaju <lanrehnics@kobo360.com>
2020-07-29 21:41:05 +0100

    --

--- lib/mobile_map_core.dart
+++ lib/mobile_map_core.dart
@@ -5,11 +5,12 @@ import 'package:flutter/services.dart';
 class MobileMapCore {
   static const MethodChannel _channel = const MethodChannel('mobile_map_core');
 
+  //Test
   static const String LAUNCH_MAP_METHOD = "launch_map";
   static const String NAVIGATE_DRIVER_METHOD = "navigate_driver";
 
-  static loadMap(Map<String, dynamic> data, {String method = LAUNCH_MAP_METHOD}) async {
+  static loadMap(Map<String, dynamic> data,
+      {String method = LAUNCH_MAP_METHOD}) async {
     await _channel.invokeMethod(method, data);
   }
-
 }
